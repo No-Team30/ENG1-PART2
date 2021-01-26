412e228ccf501308a7f214fad6eecc9ab9769cee
Sam <a>
2021-01-26 15:41:12 +0000

    Started test writing

--- core/src/test/saves/LoadGameTest.java
+++ core/src/test/saves/LoadGameTest.java
@@ -1,14 +1,36 @@
 package saves;
 
+import characters.Entities.Player;
+import characters.Movement.AiMovement;
+import characters.Movement.Movement;
+import characters.Movement.UserMovement;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import map.Map;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import screen.Gameplay;
 import screen.LoadGame;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(GdxTestRunner.class)
 class LoadGameTest {
+
+/*    private Gameplay gameplay;
+    public LoadGameTest(){
+        GameMain game =new GameMain();
+        Application test=new HeadlessApplication(game);
+        //gameplay=new Gameplay(game);
+        System.out.println("Geberated test");
+    }*/
 
     @Test
     @DisplayName("Load Object - Exist and Correct Type")
@@ -143,5 +165,73 @@ class LoadGameTest {
             assertThrows(IllegalArgumentException.class, () -> LoadGame.validateAndLoadObject(someData,
                     (String) data[0], data[1]));
         }
+    }
+
+
+    @Test
+    @DisplayName("Checks map exists")
+    void TestMap() {
+        TmxMapLoader maploader = new TmxMapLoader();
+        // load the tiled map
+        TiledMap map = maploader.load("Map/Map.tmx");
+        Map.create(map);
+        assertNotNull(map);
+    }
+
+    @Test
+    @DisplayName("Validates spawn locations exist")
+    void TestSpawn() {
+        TmxMapLoader maploader = new TmxMapLoader();
+        // load the tiled map
+        TiledMap map = maploader.load("Map/Map.tmx");
+        Map.create(map);
+        assertNotNull(map);
+        MapLayer layers = map.getLayers().get("spawn");
+        assertNotNull(layers);
+        assertNotNull(layers.getObjects().get(0));
+    }
+
+    @Test
+    @DisplayName("Save and Load Npc")
+    void TestPlayer() {
+        Player player = buildPlayer();
+        JSONObject savedPlayer = player.save();
+        Player loadedPlayer = new Player(buildWorld(), buildMap(), savedPlayer);
+        assertNull(Gameplay.player);
+    }
+
+    Boolean MovementSystemsEqual(Movement a, Movement b) {
+        return !(a instanceof AiMovement) || !(b instanceof AiMovement);
+    }
+
+    Boolean AiMovementSystemsEqual(AiMovement a, AiMovement b) {
+        if (!a.getPosition().equals(b.getPosition())) {
+            return false;
+        }
+        if (!a.)
+            return true;
+    }
+
+    Boolean AiMovementSystemsEqual(UserMovement a, UserMovement b) {
+        return !(a instanceof AiMovement) || !(b instanceof AiMovement);
+    }
+
+    World buildWorld() {
+        return new World(new Vector2(0, 0), true);
+    }
+
+    TiledMap buildMap() {
+        TmxMapLoader maploader = new TmxMapLoader();
+        // load the tiled map
+        TiledMap map = maploader.load("Map/Map.tmx");
+        Map.create(map);
+        return map;
+    }
+
+    Player buildPlayer() {
+        World world = buildWorld();
+        TiledMap map = buildMap();
+        Rectangle spawn = ((RectangleMapObject) map.getLayers().get("spawn").getObjects().get(0)).getRectangle();
+        return new Player(world, spawn.x, spawn.y, map);
     }
 }
